8133f06f8fb07ea3cb6ab70b82036f2120ce766f
7omich <b6u728@gmail.com>
2019-01-03 16:27:35 +0900

    Finish Lesson08

---
+++ SimpleC++/Lesson08: Pointers/8-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void add(int* pX1, int* pX2, int a);
+
+int main() {
+    int x1;
+    int x2;
+    int a;
+
+    cout << "2科目分の点数を入力してください。\n";
+    cin >> x1;
+    cin >> x2;
+    cout << "加算する点数を入力してください。\n";
+    cin >> a;
+    cout << a << "点加算しましたので\n";
+
+    add(&x1, &x2, a);
+    cout << "科目1は" << x1 << "点となりました。\n";
+    cout << "科目2は" << x2 << "点となりました。\n";
+
+    return 0;
+}
+
+void add(int* pX1, int* pX2, int a) {
+    *pX1 += a;
+    *pX2 += a;
+}
